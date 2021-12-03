dde70cc886aa493d148bf2b731682a212039ffad
RenKunzhao <3281634601@qq.com>
2021-12-03 11:08:48 -0800

    marker/markerArray

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -1,5 +1,5 @@
 # generated from catkin/cmake/template/order_packages.context.py.in
 source_root_dir = '/home/rkzdtc/catkin_ws/src'
-whitelisted_packages = 'scene_editor'.split(';') if 'scene_editor' != '' else []
+whitelisted_packages = 'using_markers'.split(';') if 'using_markers' != '' else []
 blacklisted_packages = ''.split(';') if '' != '' else []
 underlay_workspaces = '/home/rkzdtc/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/rkzdtc/catkin_ws/devel;/opt/ros/melodic' != '' else []
